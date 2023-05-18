878070be0dceec1e77e89777440ac333383e3f19
Ewan Smith <ewan.smith@strath.ac.uk>
2023-05-18 12:09:38 +0100

    Update metadata

--- src/site.config.ts
+++ src/site.config.ts
@@ -18,7 +18,7 @@ export const siteConfig: SiteConfig = {
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
 	title: "Ewan Smith",
 	// Meta property used as a default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "My corner of the internet",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42

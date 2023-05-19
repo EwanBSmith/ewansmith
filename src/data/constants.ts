83605076fe08f77fb39f974bef17a2bedf84f6b2
Ewan Smith <ewan.smith@strath.ac.uk>
2023-05-19 15:03:11 +0100

    Fix twitter link

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -20,7 +20,6 @@ export const MENU_LINKS = [
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
 	github: "https://github.com/EwanBSmith",
-	twitter: "https://github.com/EwanSmithPhD",
-	discord: "https://github.com/chrismwilliams/astro-cactus",
+	twitter: "https://twitter.com/EwanSmithPhD",
 	email: "me@example.com",
 };
